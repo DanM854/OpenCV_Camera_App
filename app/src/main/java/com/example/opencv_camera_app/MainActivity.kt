69137bb13b1b09b3a4388289c59f2d3de1ef65d6
Daniel <daniel.mora01@usa.edu.co>
2025-02-26 17:37:50 +0000

    Functionalities and camera permissions

--- app/src/main/java/com/example/opencv_camera_app/MainActivity.kt
+++ app/src/main/java/com/example/opencv_camera_app/MainActivity.kt
@@ -5,10 +5,30 @@ import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import org.opencv.android.OpenCVLoader
+import android.Manifest
+import android.content.pm.PackageManager
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import org.opencv.android.CameraBridgeViewBase
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.ImageView
+import org.opencv.core.CvType
+import org.opencv.core.Mat
 
-class MainActivity : AppCompatActivity() {
+
+
+class MainActivity : AppCompatActivity(), CameraBridgeViewBase.CvCameraViewListener2 {
+    private lateinit var buttonStartPreview: Button
+    private lateinit var buttonStopPreview: Button
+    private lateinit var checkBoxProcessing: Button
+    private lateinit var imageView: ImageView
+    private lateinit var openCvCameraView: CameraBridgeViewBase
+
+    private var isPreviewActive = false
     private lateinit var textViewStatus: TextView
     private var isOpenCvInitialized = false
+    private val cameraPermissionRequestCode = 100
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,8 +36,41 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         textViewStatus = findViewById(R.id.textViewStatus)
+        buttonStartPreview = findViewById(R.id.buttonStartPreview)
+        buttonStopPreview = findViewById(R.id.buttonStopPreview)
+        checkBoxProcessing = findViewById(R.id.checkboxEnableProcessing)
+        imageView = findViewById(R.id.imageView)
+        openCvCameraView = findViewById(R.id.cameraView)
+
 
         isOpenCvInitialized = OpenCVLoader.initLocal()
+
+        //acceso a camara
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+            != PackageManager.PERMISSION_GRANTED){
+            //no dio permiso, debemos solicitarlo
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.CAMERA),
+                cameraPermissionRequestCode
+            )
+        }
+
+        openCvCameraView.setCameraIndex(0)
+        openCvCameraView.setCvCameraViewListener(this)
+
+        buttonStartPreview.setOnClickListener {
+            openCvCameraView.setCameraPermissionGranted()
+            openCvCameraView.enableView()
+
+            updateControls()
+        }
+
+        buttonStopPreview.setOnClickListener {
+            openCvCameraView.disableView()
+
+            updateControls()
+        }
 
         updateControls()
     }
@@ -25,8 +78,31 @@ class MainActivity : AppCompatActivity() {
     private fun updateControls() {
         if(!isOpenCvInitialized){
             textViewStatus.text = "OpenCV initialization error"
+
+            buttonStartPreview.isEnabled = false
+            buttonStopPreview.isEnabled = false
         }else{
             textViewStatus.text = "OpenCV initialized"
+
+            buttonStartPreview.isEnabled = !isPreviewActive
+            buttonStopPreview.isEnabled = isPreviewActive
         }
+    }
+
+    override fun onCameraViewStarted(width: Int, height: Int) {
+        isPreviewActive = true
+
+        updateControls()
+    }
+
+    override fun onCameraViewStopped() {
+        isPreviewActive = false
+
+        updateControls()
+    }
+
+    override fun onCameraFrame(inputFrame: CameraBridgeViewBase.CvCameraViewFrame?): Mat {
+        return inputFrame!!.rgba()
+
     }
 }
